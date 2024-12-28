af7516f2971872e0559a4b34d0378fd7ed51c93e
nwpcgnpro <nwpcgn@gmail.com>
2024-12-28 13:48:14 +0100

    ua

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,15 @@
 <script>
-	import svelteLogo from './assets/svelte.svg';
-	import viteLogo from '/vite.svg';
-	import Counter from './lib/Counter.svelte';
+	import Tab1 from './components/Tab1.svelte'
+	import Tab2 from './components/Tab2.svelte'
+	import Tab3 from './components/Tab3.svelte'
+	import Tabs from './components/Tabs.svelte'
+	let items = [
+		{ label: 'Content', value: 1, component: Tab1 },
+		{ label: 'Interactions', value: 2, component: Tab2 },
+		{ label: 'Tab 3', value: 3, component: Tab3 }
+	]
 </script>
 
-<main>
-	<div>
-		<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-			<img src={viteLogo} class="logo" alt="Vite Logo" />
-		</a>
-		<a href="https://svelte.dev" target="_blank" rel="noreferrer">
-			<img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-		</a>
-	</div>
-	<h1>Vite + Svelte</h1>
-
-	<div class="card">
-		<Counter></Counter>
-	</div>
-
-	<p>
-		Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer"
-			>SvelteKit</a
-		>, the official Svelte app framework powered by Vite!
-	</p>
-
-	<p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
+<main class="main" data-theme="light">
+	<Tabs {items} />
 </main>
-
-<style>
-	.logo {
-		height: 6em;
-		padding: 1.5em;
-		will-change: filter;
-		transition: filter 300ms;
-	}
-	.logo:hover {
-		filter: drop-shadow(0 0 2em #646cffaa);
-	}
-	.logo.svelte:hover {
-		filter: drop-shadow(0 0 2em #ff3e00aa);
-	}
-	.read-the-docs {
-		color: #888;
-	}
-</style>
---
+++ src/components/Tab1.svelte
@@ -0,0 +1,4 @@
+<script lang="ts"></script>
+<h1>
+    Tab 1
+</h1>
